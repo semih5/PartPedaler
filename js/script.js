1f8fe62c6b3c0e52c348dc5b2e583125c37a18f5
Semih <77493886+semih5@users.noreply.github.com>
2023-04-22 01:59:37 +0300

    update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ window.addEventListener("load", function () {
   var loadingGif = document.querySelector(".loading-gif");
   loadingGif.parentNode.removeChild(loadingGif);
 });
+
+const myBikeButton = document.getElementById("myBikeButton");
+
+myBikeButton.addEventListener("click", function () {
+  window.location.href = "myBike.html";
+});
